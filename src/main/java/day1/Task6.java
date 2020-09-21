9eab5187e790bb31632262f9551770aca1212086
sboyko <your_email@whatever.comm909mk@gmail.comgit config --global user.email your_email@whatever.com>
2020-09-21 16:51:00 +0300

    complete day1 tasks

---
+++ src/main/java/day1/Task6.java
@@ -0,0 +1,11 @@
+package day1;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int k = 7;
+        for (int i =1; i < 10; i++){
+            int s = i * k;
+            System.out.println(i + " x " + k + " = " + s);
+        }
+    }
+}
