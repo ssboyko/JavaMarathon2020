69e2adffe534c6607b05703f8c26ff9fa4309d07
sboyko <your_email@whatever.comm909mk@gmail.comgit config --global user.email your_email@whatever.com>
2020-09-22 17:17:50 +0300

    complete day2 tasks

--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -1,7 +1,27 @@
 package day2;
 
+import java.util.Scanner;
+
+import static java.lang.Math.pow;
+
 public class Task4 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double x = scanner.nextDouble();
+        double y;
+        if (x>=5){
+            y = (pow(x,2)-10) / (x+7);
+            System.out.println(y);
+        }
+        if (x > -3 && x < 5){
+            y = (x+3)*(pow(x,2) - 2);
+            System.out.println(y);
+        }
+        else {
+            y=420.0;
+            System.out.println(y);
+        }
 
     }
 }
