fbb911a0c785814b54a5d62687b2988b67f82e0d
sboyko <your_email@whatever.comm909mk@gmail.comgit config --global user.email your_email@whatever.com>
2020-10-06 22:22:47 +0300

    complete day15 tasks

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,27 @@
 package day15;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("src/main/resources/shoes.csv");
+        File file1 = new File("src/main/resources/missing_shoes.txt");
+        Scanner scanner = new Scanner(file);
+        PrintWriter printWriter = new PrintWriter(file1);
+        int countofzero = 0;
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] shoes = line.split(";");
+            if (Integer.parseInt(shoes[2]) == 0) {
+                printWriter.write(Arrays.toString(shoes));
+            }
+        }
+        printWriter.close();
 
     }
 }
